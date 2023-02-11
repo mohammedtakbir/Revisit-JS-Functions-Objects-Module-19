c3a040ade16c79236a9a45e1f52ee47acc6f7751
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-11 23:20:09 +0600

    explore string.includes() and string.slice() method

---
+++ string-includes.js
@@ -0,0 +1,2 @@
+let str = 'The quick brown fox jumps over the lazy dog.';
+console.log(str.includes('Fox'))
---
+++ string-slice.js
@@ -0,0 +1,3 @@
+let str = 'The quick brown fox jumps over the lazy dog.';
+// console.log(str.slice(4, 15))
+// console.log(str.slice(-9, -2))s
